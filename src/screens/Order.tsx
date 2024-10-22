675d8660ae439a8e299f218304fc4e947dd7b3a6
ducgiangtrankma <tranducgiangact@gmail.com>
2024-10-22 15:22:28 +0700

    public

--- src/screens/Order.tsx
+++ src/screens/Order.tsx
@@ -1,19 +1,13 @@
 import React, {FC, useRef} from 'react';
-import {SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
-import {MusicAnimation, Product3dPreviewRef} from '../components';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Product3dPreview, Product3dPreviewRef} from '../components';
 
 interface Props {}
 export const Orders: FC<Props> = () => {
   const componentRef = useRef<Product3dPreviewRef>(null);
-
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        activeOpacity={1}
-        style={styles.touchable}
-        onPress={() => componentRef.current?.resetScale()}>
-        <MusicAnimation />
-      </TouchableOpacity>
+      <Product3dPreview ref={componentRef} />
     </SafeAreaView>
   );
 };
@@ -21,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: 'gray',
   },
   touchable: {
     flex: 1,
